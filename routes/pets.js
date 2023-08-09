d87166016fb3d35dc55f55de2204f5921d28f822
boone84 <boone84@Boone-PC.localdomain>
2023-08-09 16:52:15 -0400

    pet finder

--- routes/pets.js
+++ routes/pets.js
@@ -1,34 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const petsData = [
-    { name: 'Buddy', owner: 'John', type: 'Dog' },
-    { name: 'Whiskers', owner: 'Alice', type: 'Cat' },
-    { name: 'Goldie', owner: 'Steve', type: 'Fish' }
+  { name: "Buddy", owner: "John", type: "Dog" },
+  { name: "Whiskers", owner: "Alice", type: "Cat" },
+  { name: "Goldie", owner: "Steve", type: "Fish" },
 ];
 
-
-router.get('/', (req, res) => {
-    res.json(petsData);  
+router.get("/", (req, res) => {
+  res.json(petsData);
 });
 
-router.get('/:name', (req, res) => {
-    const pet = petsData.find(p => p.name === req.params.name);
-    if (pet) {
-        res.json(pet);
-    } else {
-        res.status(404).send('Pet not found');
-    }
+router.get("/:name", (req, res) => {
+  const pet = petsData.find((p) => p.name === req.params.name);
+  if (pet) {
+    res.json(pet);
+  } else {
+    res.status(404).send("Pet not found");
+  }
 });
 
-router.get('/owner', (req, res) => {
-    const ownerName = req.query.name; 
-    const pet = petsData.find(p => p.owner === ownerName);
-    if (pet) {
-        res.json(pet);
-    } else {
-        res.status(404).send('Pet not found for given owner');
-    }
+router.get("/owner", (req, res) => {
+  const ownerName = req.query.name;
+  const pet = petsData.find((p) => p.owner === ownerName);
+  if (pet) {
+    res.json(pet);
+  } else {
+    res.status(404).send("Pet not found for given owner");
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const petsRoutes = require('./routes/pets');
+const express = require("express");
+const petsRoutes = require("./routes/pets");
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));  
+app.use(express.static("public"));
 
-app.use('/pets', petsRoutes);  
+app.use("/pets", petsRoutes);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
