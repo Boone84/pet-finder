122f9210db5ddf7e615201cf3a2dc3fe24943cf9
boone84 <boone84@Boone-PC.localdomain>
2023-08-09 15:39:01 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const petsRoutes = require('./routes/pets');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));  
+
+app.use('/pets', petsRoutes);  
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
